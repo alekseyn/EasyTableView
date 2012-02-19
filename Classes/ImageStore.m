bf1dc87334ed0acd3377d0be3609d0a0e1a7d574
Aleksey Novicov <aleksey@yodelcode.com>
2012-02-19 09:52:07 -0800

    Image viewing example is now more robust in the absence of network or Flickr availability.

--- Classes/ImageStore.m
+++ Classes/ImageStore.m
@@ -34,7 +34,7 @@
 		NSOperationQueue *queue = [[NSOperationQueue alloc] init];
 		
 		[queue addOperationWithBlock:^{
-			NSDictionary *json;
+			NSDictionary *json = nil;
 			NSError *error;
 			
 			// Due to the high frequency of JSON deserialization errors, loop until there is no error
@@ -45,34 +45,44 @@
 				NSURL *url = [NSURL URLWithString:@"http://api.flickr.com/services/feeds/photos_public.gne?format=json&nojsoncallback=1"];
 				NSData *listData = [NSData dataWithContentsOfURL:url];
 			
-				json = [NSJSONSerialization JSONObjectWithData:listData options:NSJSONReadingAllowFragments error:&error];
-
-				// Make sure to call delegate on main queue
-				[[NSOperationQueue mainQueue] addOperationWithBlock:^{
-					if ([aDelegate respondsToSelector:@selector(errorMessage:)]) {
-						if (error)
-							[aDelegate errorMessage:[NSString stringWithFormat:@"%@ Retrying...", [error localizedDescription]]];
-						else
-							[aDelegate errorMessage:nil];
-					}
-				}];
+				// If there is no network connection, listData will be nil
+				if (listData) {
+					json = [NSJSONSerialization JSONObjectWithData:listData options:NSJSONReadingAllowFragments error:&error];
+					
+					// Make sure to call delegate on main queue
+					[[NSOperationQueue mainQueue] addOperationWithBlock:^{
+						if ([aDelegate respondsToSelector:@selector(errorMessage:)]) {
+							if (error)
+								[aDelegate errorMessage:[NSString stringWithFormat:@"%@ Retrying...", [error localizedDescription]]];
+							else
+								[aDelegate errorMessage:nil];
+						}
+					}];
+				}
+				else {
+					if ([aDelegate respondsToSelector:@selector(errorMessage:)])
+						[aDelegate errorMessage:@"Internet is not available. Please try again later."];
+				}
 			}
 			while (error);
 
-			self.titles = [json valueForKeyPath:@"items.title"];
-			self.urls = [json valueForKeyPath:@"items.media.m"];
-			
-			// Setup image cache
-			NSMutableDictionary *cache = [[NSMutableDictionary alloc] initWithCapacity:[self.titles count]];
-			self.imageCache = cache;
-			[cache release];
-			
-			// Make sure to call delegate on main queue
-			[[NSOperationQueue mainQueue] addOperationWithBlock:^{
-				if ([aDelegate respondsToSelector:@selector(imageTitles:)]) {
-					[aDelegate imageTitles:self.titles];
-				}
-			}];
+			// If there is no network connection, json will be nil
+			if (json) {
+				self.titles = [json valueForKeyPath:@"items.title"];
+				self.urls = [json valueForKeyPath:@"items.media.m"];
+				
+				// Setup image cache
+				NSMutableDictionary *cache = [[NSMutableDictionary alloc] initWithCapacity:[self.titles count]];
+				self.imageCache = cache;
+				[cache release];
+				
+				// Make sure to call delegate on main queue
+				[[NSOperationQueue mainQueue] addOperationWithBlock:^{
+					if ([aDelegate respondsToSelector:@selector(imageTitles:)]) {
+						[aDelegate imageTitles:self.titles];
+					}
+				}];
+			}
 		}];
 		
 		self.operationQueue = queue;
@@ -93,14 +103,16 @@
 			UIImage *newImage = [UIImage imageWithData:imageData];
 			
 			// Save the image in the image cache
-			[self.imageCache setObject:newImage forKey:urlString];
-			
-			// Make sure to call delegate on main queue
-			[[NSOperationQueue mainQueue] addOperationWithBlock:^{
-				if ([self.delegate respondsToSelector:@selector(image:loadedAtIndex:)]) {
-					[self.delegate image:newImage loadedAtIndex:index];
-				}
-			}];
+			if (newImage) {
+				[self.imageCache setObject:newImage forKey:urlString];
+				
+				// Make sure to call delegate on main queue
+				[[NSOperationQueue mainQueue] addOperationWithBlock:^{
+					if ([self.delegate respondsToSelector:@selector(image:loadedAtIndex:)]) {
+						[self.delegate image:newImage loadedAtIndex:index];
+					}
+				}];
+			}
 		}];
 	}
 	return image;
